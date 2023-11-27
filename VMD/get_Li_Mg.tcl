0f765d99a454e3b4f5d5c586a51b257a82986fc3
luck19990920 <124136999+luck19990920@users.noreply.github.com>
2023-11-27 18:32:15 +0800

    Update get_Li_Mg.tcl

--- VMD/get_Li_Mg.tcl
+++ VMD/get_Li_Mg.tcl
@@ -1,8 +1,13 @@
+# A script to calculate  number of particles passing through the pore in molecular simulation
+# Variable bar is boundary z-coordinate of membrane. Please fix its value according to the actual situation 
+# Variable num is the number of particles
+# In this script, the particle number to be counted starts from 0. Please replace 0 in Line 9 to actual value according to the actual situation  
 set n [ molinfo top get numframes ] 
 set Li_across 0
 set bar 55
+set num 8
 for { set i 0 } { $i < [ expr $n-1 ] } { incr i } {
-    for { set j 0 } { $j <= 1706 } { incr j } {
+    for { set j 0 } { $j <= $num } { incr j } {
         set Li_t1 [ atomselect top "index $j" frame $i ]
         set Li_t2 [ atomselect top "index $j" frame [expr $i+1 ] ]
         if { [ $Li_t1 get z ] <= $bar && [ $Li_t2 get z ] >= $bar } {
